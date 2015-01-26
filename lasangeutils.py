f6cceddf937d162fa6f886cf4f57db82d2157535
Felix Lau <felixlaumon@gmail.com>
2015-01-26 16:56:31 +0000

    updates

--- lasangeutils.py
+++ lasangeutils.py
@@ -94,3 +94,54 @@ def random_rotate_img(img, angle_range=360):
     # Fill "blank space" with white because the images have white background
     # instead
     return rotate(img, angle, cval=1.0)
+
+
+def im_rotate(img, angle):
+    return skimage.transform.rotate(img, angle, mode='reflect')
+
+
+def im_translate(img, shift_x, shift_y):
+    ## this could probably be a lot easier... meh.
+    # downsampling afterwards is recommended
+    translate_img = np.ones_like(img, dtype=img.dtype)
+
+    if shift_x >= 0:
+        slice_x_src = slice(None, img.shape[0] - shift_x, None)
+        slice_x_tgt = slice(shift_x, None, None)
+    else:
+        slice_x_src = slice(- shift_x, None, None)
+        slice_x_tgt = slice(None, img.shape[0] + shift_x, None)
+
+    if shift_y >= 0:
+        slice_y_src = slice(None, img.shape[1] - shift_y, None)
+        slice_y_tgt = slice(shift_y, None, None)
+    else:
+        slice_y_src = slice(- shift_y, None, None)
+        slice_y_tgt = slice(None, img.shape[1] + shift_y, None)
+
+    translate_img[slice_x_tgt, slice_y_tgt] = img[slice_x_src, slice_y_src]
+
+    return translate_img
+
+
+def im_rescale(img, scale_factor):
+    zoomed_img = np.ones_like(img, dtype=img.dtype)
+    zoomed = skimage.transform.rescale(img, scale_factor)
+
+    if scale_factor >= 1.0:
+        shift_x = (zoomed.shape[0] - img.shape[0]) // 2
+        shift_y = (zoomed.shape[1] - img.shape[1]) // 2
+        zoomed_img[:,:] = zoomed[shift_x:shift_x+img.shape[0], shift_y:shift_y+img.shape[1]]
+    else:
+        shift_x = (img.shape[0] - zoomed.shape[0]) // 2
+        shift_y = (img.shape[1] - zoomed.shape[1]) // 2
+        zoomed_img[shift_x:shift_x+zoomed.shape[0], shift_y:shift_y+zoomed.shape[1]] = zoomed
+
+    return zoomed_img
+
+
+def im_lcn(img, radius=30):
+    """http://scikit-image.org/docs/dev/auto_examples/plot_local_equalize.html"""
+    selem = skimage.morphology.disk(radius)
+    img_eq = skimage.filter.rank.equalize(img, selem=selem)
+    return img_eq
